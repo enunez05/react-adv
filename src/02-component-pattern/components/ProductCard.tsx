6dba94682850ec6d9f8eee2247d4b8a1bdcb53b7
Eduardo Nuñez <eduardonunez05@gmail.com>
2022-08-26 16:57:03 -0600

    starting project

---
+++ src/02-component-pattern/components/ProductCard.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import noImage from '../assets/np-image.jpg'
+import styles from '../styles/styles.module.css';
+
+export const ProductCard = () => {
+
+    const [counter, setCounter] = useState(0)
+
+    const increaseBy = (value:number) => {
+        setCounter( prev=> Math.max(prev + value, 0) )
+    }
+
+  return (
+    <div className={styles.productCard}>
+        <img className={styles.productImg} src="./coffee-mug.png" alt="Coffe Mug" />
+        {/* <img className={styles.productImg} src={noImage} alt="Coffe Mug" /> */}
+
+        <span className={styles.productDescription}>Coffe Mug</span>
+
+        <div className={styles.buttonsContainer}>
+            <button
+                className={styles.buttonMinus}
+                onClick={()=> increaseBy(-1)}
+            >
+            -
+            </button>
+            <div className={styles.countLabel}>{counter}</div>
+            <button className={styles.buttonAdd} onClick={()=> increaseBy(1)}>+</button>
+        </div>
+
+    </div>
+  )
+}
