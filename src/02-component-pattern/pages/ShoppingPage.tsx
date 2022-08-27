db984020779115aacf3eac5dc76df6b8f6427824
Eduardo Nuñez <eduardonunez05@gmail.com>
2022-08-26 19:25:53 -0600

    Compound  Component

--- src/02-component-pattern/pages/ShoppingPage.tsx
+++ src/02-component-pattern/pages/ShoppingPage.tsx
@@ -1,11 +1,36 @@
-import {ProductCard} from '../components/ProductCard';
+import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components/ProductCard';
+
+const product = {
+    id:1,
+    title: 'Coffe Mug - Card',
+    img:'./coffee-mug.png'
+}
+
 
 export const ShoppingPage = () => {
   return (
     <div>
         <h1>ShoppingPage</h1>
         <hr />
-        <ProductCard />
+
+        <div style={{
+            display:'flex',
+            flexWrap:'wrap',
+            gap:'1rem'
+        }}>
+            <ProductCard product={product}>
+                <ProductCard.Image />
+                <ProductCard.Title title="Cafe" />
+                <ProductCard.Buttons />
+            </ProductCard>
+
+            <ProductCard product={product}>
+                <ProductImage />
+                <ProductTitle />
+                <ProductButtons />
+            </ProductCard>
+        </div>
+
     </div>
 
   )
